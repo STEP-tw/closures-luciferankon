5b4d4960695399f1d7a57e8c7aa055b1d7ad1e2e
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-10-25 12:06:25 +0530

    implemented generateFibNumber function

--- src/lib.js
+++ src/lib.js
@@ -15,7 +15,37 @@ const makeCounterFromZero = function(){
   }
 }
 const makeDeltaTracker = undefined;
-const makeFiboGenerator = undefined;
+//  return function(delta){
+//    let result = {old:'',delta:'',new:''};
+//    if(result.old=='' || delta==undefined){
+//      result = {old:old, delta:0, new:old}
+//      if(delta!=undefined){
+//        result = {old:old, delta:delta, new:old+delta};
+//      }
+//      return result;
+//    }
+//    result={old:old+delta, delta:delta, new:old+delta};
+//    return result;
+//  }
+//}
+const makeFiboGenerator = function(secondElement,firstElement){
+  if(firstElement!=undefined && secondElement!=undefined){
+    let temp = firstElement;
+    firstElement = secondElement;
+    secondElement = temp;
+  }
+  if(firstElement==undefined){
+    if(secondElement==undefined){
+      secondElement = 1;
+    }
+    firstElement = 0;
+  }
+  let fibNumber = { fibValue: 0, oldValue:firstElement, newValue:secondElement};
+  return function(){
+    fibNumber = { fibValue: fibNumber.oldValue, oldValue: fibNumber.newValue, newValue:fibNumber.oldValue+fibNumber.newValue};
+    return fibNumber.fibValue;
+  }
+}
 const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
