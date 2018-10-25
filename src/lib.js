b7d1ee5b95cf6161f5d10db5279ef43884bc2640
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-10-25 11:01:45 +0530

    implemented countFromZero function

--- src/lib.js
+++ src/lib.js
@@ -8,7 +8,12 @@ const makeCounterFromN = function(number){
     return number++;
   }
 }
-const makeCounterFromZero = undefined;
+const makeCounterFromZero = function(){
+  let count = 0;
+  return function(){
+    return count++;
+  }
+}
 const makeDeltaTracker = undefined;
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
